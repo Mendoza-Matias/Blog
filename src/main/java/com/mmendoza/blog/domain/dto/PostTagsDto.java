e0c8a158c808ce37a83f480e7142a99e04297a09
Matias Mendoza <mendoza.e.matias@gmail.com>
2025-04-10 20:38:59 -0300

    creo dto para dos endpoint de postController

--- src/main/java/com/mmendoza/blog/domain/dto/PostTagsDto.java
+++ src/main/java/com/mmendoza/blog/domain/dto/PostTagsDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class AddTagToPostDto {
+public class PostTagsDto {
     private Integer postId;
     private List<Integer> tagIds;
 }
